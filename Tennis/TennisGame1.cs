2a2ea869999b1e5df8ef48deee46224471626f86
Hugo Biarge <hugo@arosbi.com>
2018-04-02 19:38:29 +0200

    Initial commit

---
+++ Tennis/TennisGame1.cs
@@ -0,0 +1,83 @@
+using System;
+
+namespace Tennis
+{
+    class TennisGame1 : ITennisGame
+    {
+        private int m_score1 = 0;
+        private int m_score2 = 0;
+        private string player1Name;
+        private string player2Name;
+
+        public TennisGame1(string player1Name, string player2Name)
+        {
+            this.player1Name = player1Name;
+            this.player2Name = player2Name;
+        }
+
+        public void WonPoint(string playerName)
+        {
+            if (playerName == "player1")
+                m_score1 += 1;
+            else
+                m_score2 += 1;
+        }
+
+        public string GetScore()
+        {
+            string score = "";
+            if (m_score1 == m_score2)
+            {
+                switch (m_score1)
+                {
+                    case 0:
+                        score = "Love-All";
+                        break;
+                    case 1:
+                        score = "Fifteen-All";
+                        break;
+                    case 2:
+                        score = "Thirty-All";
+                        break;
+                    default:
+                        score = "Deuce";
+                        break;
+
+                }
+            }
+            else if (m_score1 >= 4 || m_score2 >= 4)
+            {
+                var minusResult = m_score1 - m_score2;
+                if (minusResult == 1) score = "Advantage player1";
+                else if (minusResult == -1) score = "Advantage player2";
+                else if (minusResult >= 2) score = "Win for player1";
+                else score = "Win for player2";
+            }
+            else
+            {
+                score = GetPlayerScore(m_score1);
+                score += "-";
+                score += GetPlayerScore(m_score2);
+            }
+            return score;
+        }
+
+        private static string GetPlayerScore(int score)
+        {
+            switch (score)
+            {
+                case 0:
+                    return "Love";
+                case 1:
+                    return "Fifteen";
+                case 2:
+                    return "Thirty";
+                case 3:
+                    return "Forty";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(score));
+            }
+        }
+    }
+}
+
